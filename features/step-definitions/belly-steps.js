90d2a682e0191bacd81ce4cfe07ebcae28b7d7d9
maritvandijk <mlvandijk@gmail.com>
2018-02-17 08:33:36 +0100

    Add step definitions

---
+++ features/step-definitions/belly-steps.js
@@ -0,0 +1,17 @@
+const { Before, Given, When, Then } = require('cucumber')
+
+Given('I have {int} cukes in my belly', function (int, callback) {
+// Write code here that turns the phrase above into concrete actions
+callback(null, 'pending');
+});
+
+
+When('I wait {int} hour', function (int, callback) {
+// Write code here that turns the phrase above into concrete actions
+callback(null, 'pending');
+});
+
+Then('my belly should growl', function (callback) {
+// Write code here that turns the phrase above into concrete actions
+callback(null, 'pending');
+});
